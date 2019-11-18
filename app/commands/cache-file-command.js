5b8a52aec2952849155c393f234111f46f7ec29f
plouffed <daveplouffe@hotmail.com>
2019-11-18 09:27:37 -0500

    no need for new operator

--- app/commands/cache-file-command.js
+++ app/commands/cache-file-command.js
@@ -5,8 +5,7 @@ const cacheFile = {
    * @param {Linkus} linkus
    */
   execute(linkus) {
-    let cached = new DependencyCached(linkus);
-    cached.saveFile(linkus.context.curFile.fileInfo.ino, linkus.context.curFile.content);
+    linkus.cached.saveFile(linkus.context.curFile.fileInfo.ino, linkus.context.curFile.content);
   }
 };
 
